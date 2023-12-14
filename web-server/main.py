6711323bebcd27152e2aa81aeabdd60aba3ed5e9
Yurakc <luis8866@hotmail.com>
2023-12-14 01:13:23 -0500

    project web server add fastapi and uvicorn server web up

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,28 @@
 import store
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Yurak Web</title>
+        </head>
+        <body>
+            <h1>Mi primer servidor web!</h1>
+            <p>Yurak aparece en la web :)</p>
+        </body>
+    </html>
+    """
+
 def run():
     store.get_categories()
 
